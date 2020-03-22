be2e0ee51fa5d64c3606dc3341f70f36f2bc6de5
Scott Little <k1206150@students.katyisd.org>
2020-03-22 14:50:12 -0500

    Add C command

--- src/term.py
+++ src/term.py
@@ -4,6 +4,12 @@ while True:
   line = raw_input("$ ").split(" ")
   if line[0] == 'l':
     if len(line) < 3:
-      #
+      continue
     else:
        io.list()
+  if line[0] == 'c':
+    if len(line) != 3:
+      print("E: Argument mismatch: c <src> <dest>")
+    else:
+      copy(line[1], line[2])
+
