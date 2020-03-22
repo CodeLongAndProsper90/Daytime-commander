2cdf952716e5fff8602ef06cbd8934eaffdb7b8d
Scott Little <k1206150@students.katyisd.org>
2020-03-22 14:16:58 -0500

    TO work

--- src/io.py
+++ src/io.py
@@ -4,7 +4,7 @@ python io.py [flags] directory
 
 import sys
 from os import walk
-
+from os import remove
 files = []
 for (dirpath, dirnames, filenames) in walk(sys.argv[-1]):
     files.extend(filenames)
@@ -14,5 +14,8 @@ def copy(src, dest):
   import shutil
   shutil.copy(src, dest)
 
-def move():
+def move(src, dest):
+ shutil.move(src, dest)
 
+def delete(src):
+  os.remove(src)
